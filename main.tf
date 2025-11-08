f518ace7ad99b97af483fd0e2523e010d4caeefb
Oleksandr <olexandr.boretskyi@gmail.com>
2025-11-08 17:53:06 +0100

    add main

---
+++ main.tf
@@ -0,0 +1,8 @@
+module "dns_deployment" {
+  source = "/modules"
+  target_platform = var.target_platform
+  domain_name = var.domain_name
+  record_name     = var.record_name
+  record_type     = var.record_type
+  record_value    = var.record_value
+}
