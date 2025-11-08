36b97f475ca7009447fd38205cc2dc2a4fbad624
Oleksandr <olexandr.boretskyi@gmail.com>
2025-11-08 17:10:02 +0100

    fix variables

---
+++ modules/variables.tf
@@ -0,0 +1,29 @@
+variable "domain_name" {
+  description = "Domain name"
+  type        = string
+}
+
+variable "target_platform" {
+  description = "DNS platform to use ('AWS' or 'cloudflare')"
+  type        = string
+}
+
+variable "record_name" {
+  description = "DNS name"
+  type        = string
+}
+
+variable "record_type" {
+  description = "DNS type"
+  type        = string
+}
+
+variable "record_value" {
+  default = "DNS record value"
+  type    = string
+}
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare Zone ID"
+  type        = string
+}
